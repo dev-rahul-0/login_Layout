14ba23fd2b7c18038ed29c004bcef70b01c84b20
dev-rahul-0 <rs650417@gmail.com>
2024-02-14 20:21:34 +0530

    update

--- lib/layout.dart
+++ lib/layout.dart
@@ -13,84 +13,101 @@ class _LayoutState extends State<Layout> {
     return Scaffold(
       body: Stack(
         children: [
-          Column(
-            children: [
-              Container(
-                child: const Image(
-                  image: NetworkImage(
-                      'https://www.rkinfotechindia.com/wp-content/uploads/2018/11/ui-ux-design-img.png'),
-                ),
-                height: 400,
-                width: 500,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(25),
-                  boxShadow: [
-                    const BoxShadow(
-                      color: Colors.deepPurple,
-                      offset: Offset(0.0, 8.0), //(x,y)
-                      blurRadius: 6.0,
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 25, right: 200),
-                child: Text(
-                  "Login",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 40,
-                      color: Colors.deepPurple),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                  height: 50,
-                  width: 320,
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintText: 'Username',
-                        contentPadding: const EdgeInsets.only(left: 25),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(30))),
-                  )),
-              const SizedBox(
-                height: 8,
-              ),
-              SizedBox(
-                  height: 50,
-                  width: 320,
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintText: 'Password',
-                        contentPadding: const EdgeInsets.only(left: 25),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(30))),
-                  )),
-              const SizedBox(
-                height: 15,
-              ),
-              ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(minimumSize: Size(320, 50),
-                    backgroundColor: Theme.of(context).colorScheme.inversePrimary
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  child: const Image(
+                    image: NetworkImage(
+                        'https://www.rkinfotechindia.com/wp-content/uploads/2018/11/ui-ux-design-img.png'),
                   ),
-                  child: Text('Sign in')),
-              ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(minimumSize: Size(320, 50),
-                      backgroundColor: Theme.of(context).colorScheme.inversePrimary
+                  height: 400,
+                  width: 500,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(25),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.deepPurple,
+                        offset: Offset(0.0, 8.0), //(x,y)
+                        blurRadius: 6.0,
+                      ),
+                    ],
                   ),
-                  child: Text('Sign in'))
-            ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 25, right: 200),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                        color: Colors.deepPurple),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                    height: 50,
+                    width: 320,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Username',
+                          contentPadding: const EdgeInsets.only(left: 25),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(color: Colors.grey),
+                              borderRadius: BorderRadius.circular(30))),
+                    )),
+                const SizedBox(
+                  height: 8,
+                ),
+                SizedBox(
+                    height: 50,
+                    width: 320,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Password',
+                          contentPadding: const EdgeInsets.only(left: 25),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(color: Colors.grey),
+                              borderRadius: BorderRadius.circular(30))),
+                    )),
+                const SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: Size(320, 50),
+                        backgroundColor:
+                            Theme.of(context).colorScheme.inversePrimary),
+                    child: Text('Sign in')),
+                SizedBox(
+                  height: 25,
+                ),
+                RichText(
+                    text: TextSpan(
+                        text: "Don't have Account? ",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Theme.of(context).colorScheme.inversePrimary,
+                            fontWeight: FontWeight.bold),
+                    children: const [
+                      TextSpan(
+                        text: 'Register Now',
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.purple,
+                            fontWeight: FontWeight.w600)
+                      )
+                    ]),)
+            
+              ],
+            ),
           )
         ],
       ),
